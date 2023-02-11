d74c9ebf8465a2a99321f36fad9038a6dc1e5997
riltonbispo <riltonbispo1@gmail.com>
2023-02-10 21:15:19 -0300

    feat: montando estrutura base do projeto
    - Adicionei a pasta assets com as imagens
    
    - Estruturei o app e seu estilo

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import styles from "./App.module.css";
+import powerImage from "./assets/powered.png";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className={styles.main}>
+      <header>
+        <div className={styles.headerContainer}>
+          <img src={powerImage} alt="" width={150} />
+        </div>
       </header>
+      <div className={styles.container}>
+        <div className={styles.leftSide}>esquerda</div>
+        <div className={styles.rightSide}>direita</div>
+      </div>
     </div>
   );
 }
